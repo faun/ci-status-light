b816bc27b263ae924f36424cdb8d60830c58cbe6
Faun <github@faun.me>
2017-08-22 15:18:18 -0700

    Add Install script (#1)
    * Add installation script
    
    * Move systemd example directory
    
    * Ignore systemd directory
    
    * Add systemd setup commands
    
    * Add environment variable for post-to-slack
    
    * Update example name

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [[ -d "$DIR/systemd/" ]]
+then
+  for file in $DIR/systemd/*
+  do
+    target=$(basename $file)
+    if [[ -L /etc/systemd/system/$target ]]
+    then
+      sudo rm -i /etc/systemd/system/$target
+    fi
+    echo "Linking $target to /etc/systemd/system/"
+    sudo cp -Ri "$file" /etc/systemd/system/
+  done
+  sudo systemctl daemon-reload
+  sudo systemctl stop ci-status-light
+  sudo systemctl start ci-status-light
+  sudo systemctl status ci-status-light -l
+else
+  cp -R "$DIR/systemd_examples/" "$DIR/systemd/"
+  echo "Please edit the files in "$DIR/systemd/" and run this command again"
+fi
