444b6e497a6869b6650c8c6f912692a69348a7a9
Faun <github@faun.me>
2017-08-22 22:05:57 +0000

    Move systemd example directory

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -R "$DIR/systemd/" /etc/systemd/system/
+if [[ -d "$DIR/systemd/" ]]
+then
+  for file in $DIR/systemd/*
+  do
+    target=$(basename $file)
+    if [[ -L /etc/systemd/system/$target ]]
+    then
+      sudo rm -i /etc/systemd/system/$target
+    fi
+    echo "Linking $target to /etc/systemd/system/"
+    sudo cp -Ri "$file" /etc/systemd/system/
+  done
+else
+  cp -R "$DIR/systemd_examples/" "$DIR/systemd/"
+  echo "Please edit the files in "$DIR/systemd/" and run this command again"
+fi
